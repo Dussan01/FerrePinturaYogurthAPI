1129b61d4bfee0ea2ac74aea625340e960d26226
dussan01 <kevin.dussan3@gmail.com>
2022-08-02 22:39:27 -0500

    Limpieza

--- src/routes/nota.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import app from "../app";
-const router = Router();
-import * as notaCtrl from '../controllers/nota.controller';
-
-router.get('/ver/:idUser', notaCtrl.verNotas);
-
-
-export default router;
