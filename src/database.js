c8f80d3eba0fc18e211883f2dc71e8987bce06f7
Dussan01 <global-kevin.dussan3@gmail.com>
2022-11-21 14:02:52 -0500

    Commit parcial

--- src/database.js
+++ src/database.js
@@ -2,54 +2,26 @@ import mongoose from "mongoose";
 
 
     
-mongoose.connect("mongodb://localhost/ferre",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true,
-    useCreateIndex: true
-})
-                .then(db => console.log('Db esta conectadad'))
-                .catch(error => console.log(error))
-
-                
+// mongoose.connect("mongodb://localhost/ferre",{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: true,
+//     useCreateIndex: true
+// })
+//                 .then(db => console.log('Db esta conectadad'))
+//                 .catch(error => console.log(error))
 
 
-
-
-    // try {
-
-        
-        
-    //     // mongoose.connect(
-    //     //     `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ymr7x.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
-    //     //     // `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ymr7x.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
-    //     //     ,{
-    //     //     useNewUrlParser: true,
-    //     //     useUnifiedTopology: true,
-    //     //     useFindAndModify: true,
-    //     //     useCreateIndex: true
-    //     // }
-    //     // ).then(db => console.log('Base de datos conectada')).catch(error => console.log(error))
-
-     
-
-    // } catch (error) {
-    //     console.log(error)    
-    // }
-
- 
-
-    // try {
+    try {
+        mongoose.connect(
+            `mongodb+srv://${process.env.USERDB}:${process.env.PASSWORD}@cluster0.ymr7x.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: true,
+            useCreateIndex: true
+        }
+        ).then(db => console.log('Base de datos conectada')).catch(error => console.log(error))
     
-    //     mongoose.connect(
-    //         `mongodb+srv://${process.env.USERDB}:${process.env.PASSWORD}@cluster0.ymr7x.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`,{
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true,
-    //         useFindAndModify: true,
-    //         useCreateIndex: true
-    //     }
-    //     ).then(db => console.log('Base de datos conectada')).catch(error => console.log(error))
-    
-    // } catch (error) {
-    //     console.log(error)    
-    // }
+    } catch (error) {
+        console.log(error)
+    }
